cf30d13e952b43ba7ccf37d8b00546037721f515
leilei <1101668549@qq.com>
2021-08-23 10:33:15 +0800

    feat: pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       repo: "git@github.com:docterlei/notes-h5.git",
       path: "/workspace/notes-h5",
       "post-deploy":
-        "git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js", //
+        "git reset --hard && git checkout main && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js", //
       env: {
         NODE_ENV: "production",
       },
