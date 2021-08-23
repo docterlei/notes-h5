ad48e49b763867ff594e9e0197cf29b1efdf34cc
leilei <1101668549@qq.com>
2021-08-23 12:11:13 +0800

    feat: pm2

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 const MODE = import.meta.env.MODE; // 环境变量
 
 export const baseUrl =
-  MODE == "development" ? "/api" : "http://47.111.242.149:5021";
+  MODE == "development" ? "/api" : "http://47.111.242.149:7001";
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ import { Toast } from "zarm";
 const MODE = import.meta.env.MODE; // 环境变量
 
 axios.defaults.baseURL =
-  MODE == "development" ? "/api" : "http://47.111.242.149:5021";
+  MODE == "development" ? "/api" : "http://47.111.242.149:7001";
 axios.defaults.withCredentials = true;
 axios.defaults.headers["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.headers["Authorization"] = `${
